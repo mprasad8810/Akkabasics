673f3c5288eb7d3ebad459b19df878a241300992
mprasad8810 <86431488+mprasad8810@users.noreply.github.com>
2022-08-10 13:21:31 +0530

    Basic Behavior of actormodel 1.actorsystem 2.actroRef creat second actor using spawn 3.newRecevieBuilder messages build

---
+++ src/main/java/FirstSimpleBehavior.java
@@ -0,0 +1,43 @@
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+public class FirstSimpleBehavior extends AbstractBehavior<String>{
+
+    private FirstSimpleBehavior(ActorContext<String> context) {
+        super(context);
+    }
+
+    public static Behavior<String> create(){
+//        return Behaviors.setup(context ->{
+//           return new FirstSimpleBehavior(context);
+//        });
+      return Behaviors.setup(FirstSimpleBehavior::new);
+    }
+
+    @Override
+    public Receive<String> createReceive() {
+        return newReceiveBuilder()
+                .onMessageEquals("say hello",() ->{
+                    System.out.println("Hello");
+                    return this;
+                })
+                .onMessageEquals("who are you",() ->{
+                    System.out.println("my path is"+getContext().getSelf().path());
+                    return this;
+                })
+                .onMessageEquals("create a child",()->{
+                   ActorRef<String> secondActor = getContext().spawn(FirstSimpleBehavior.create(),"secondActor");
+                   secondActor.tell("who are you");
+                   return this;
+                })
+                .onAnyMessage(message ->{
+                    System.out.println("I have received the message : "+message);
+                    return this;
+                })
+                .build();
+    }
+}
